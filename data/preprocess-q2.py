3903058cf6c29df2e241c48c34838556481343ae
Eric Han <Eric.aka.darth_vader@hotmail.com>
2020-03-15 12:58:17 +0800

    Completed Q2

--- data/preprocess-q2.py
+++ data/preprocess-q2.py
@@ -18,12 +18,15 @@ for a in range(100):
         s = a
         e = a + 9
     age_rev[str(a)] = f"{s}-{e}"
-    age_accepted.append(age_rev[str(a)])
-
-print(age_rev)
+    if not age_rev[str(a)] in age_accepted:
+        age_accepted.append(age_rev[str(a)])
 
 c = 0
 age_sex_dict = {
+    'male' : defaultdict(int),
+    'female' : defaultdict(int)
+}
+death_dict = {
     'male' : defaultdict(int),
     'female' : defaultdict(int)
 }
@@ -35,29 +38,45 @@ age_fix = {
     '0-6':'0-9'
 }
 age_set = set()
-for r in csv_to_dicts("COVID19_open_line_list"):
-    sex = r['sex']
+na = set(["N/A", "NA", "na"])
+#for r in csv_to_dicts("COVID19_open_line_list"):
+for r in csv_to_dicts("COVID19_line_list_data"):
+    sex = r['gender']
     if r['age'] and sex:
 
         a = r['age']
-        if a == "N/A" or a == "NA":
+        if a in na:
             continue
+        if sex in na:
+            continue
+
         a = age_fix[a] if a in age_fix else a
         a = age_rev[a] if a in age_rev else a
 
         if not a in age_accepted:
             continue
+
+        #print(r['outcome'])
+        #print(r['date_death_or_discharge'])
+
         age_sex_dict[sex.lower()][a] += 1
+        if r["death"] != '0':
+            death_dict[sex.lower()][a] += 1
         c += 1
 
 with open('q2.csv', 'w', newline='') as csvfile:
-    writer = csv.DictWriter(csvfile, fieldnames=["group", 'male', 'female'])
+    writer = csv.DictWriter(csvfile, fieldnames=["group", 'male', 'female', 'male_deaths', 'female_deaths'])
     writer.writeheader()
 
+    s = 0
     for aa in age_accepted:
         d = {
             'group':aa,
             'male':age_sex_dict['male'][aa],
-            'female':age_sex_dict['female'][aa]
+            'female':age_sex_dict['female'][aa],
+            'male_deaths':death_dict['male'][aa],
+            'female_deaths':death_dict['female'][aa],
         }
+        s += age_sex_dict['male'][aa] + age_sex_dict['female'][aa]
         writer.writerow(d)
+    print(s)
